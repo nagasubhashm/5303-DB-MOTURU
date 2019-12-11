5363cccfadcdf73cd0ac20363ca17abcacb0c8cb
Nagasubhash Moturu <54614130+nagasubhashm@users.noreply.github.com>
2019-12-11 13:55:52 -0600

    Add files via upload

---
+++ A09/messages/mg.py
@@ -0,0 +1,19 @@
+import markovify
+import pymongo  # package for working with MongoDB
+
+#client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# Get raw text as string.
+with open("11.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+# Print five randomly-generated sentences
+for i in range(5):
+    print(text_model.make_sentence())
+
+# Print three randomly-generated sentences of no more than 280 characters
+for i in range(3):
+    print(text_model.make_short_sentence(280))
