3dac7cf764fdfd9734281566c6559dcc334d00b8
Nagasubhash Moturu <54614130+nagasubhashm@users.noreply.github.com>
2019-12-11 14:07:54 -0600

    Update mg.py

--- A09/messages/mg.py
+++ A09/messages/mg.py
@@ -11,9 +11,9 @@ with open("11.txt") as f:
 text_model = markovify.Text(text)
 
 # Print five randomly-generated sentences
-for i in range(5):
-    print(text_model.make_sentence())
+#for i in range(5):
+#    print(text_model.make_sentence())
 
-# Print three randomly-generated sentences of no more than 280 characters
-for i in range(3):
-    print(text_model.make_short_sentence(280))
+# Print three randomly-generated sentences of no more than 50 characters
+for i in range(100000):
+    print(text_model.make_short_sentence(50))
